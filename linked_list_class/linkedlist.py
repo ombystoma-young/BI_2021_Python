b8124b0d3ab63302671d22464a4c89d89db4b593
ombystoma-young <lucnotluck@mail.ru>
2022-05-11 22:25:45 +0300

    linkedlist.py added, that contains LinkedList class.
    Also test.py added contains tests for LinkedList methods (add, extend, pop, remove_last_occurrence, first and last).

---
+++ linked_list_class/linkedlist.py
@@ -0,0 +1,147 @@
+class LinkedListItem:
+    def __init__(self, value):
+        self.value = value
+        self.__next = None
+
+    def get_next(self):
+        return self.__next
+
+    def set_next(self, next_item):
+        self.__next = next_item
+
+    def has_next(self):
+        return self.__next is not None
+
+
+class LinkedList:
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+        self.__len = 0
+
+    def __getitem__(self, index):
+        current = self.__head
+        if index < 0:
+            raise IndexError
+        for _ in range(index):
+            if current is None or not current.has_next():
+                raise IndexError
+            current = current.get_next()
+        return current.value
+
+    def __len__(self):
+        return self.__len
+
+    def __contains__(self, item):
+        for index in range(self.__len):
+            current = self.__getitem__(index)
+            if item == current:
+                return True
+        return False
+
+    def __repr__(self):
+        representative_list = []
+        current = self.__head
+        representative_list.append(current.value)
+        for _ in range(self.__len - 1):
+            current = current.get_next()
+            representative_list.append(current.value)
+        return repr(representative_list)
+
+    def add(self, value, position=None):
+        if position is None:
+            self.__len += 1
+            new_item = LinkedListItem(value)
+            if not self.__head:
+                self.__head = new_item
+            else:
+                self.__tail.set_next(new_item)
+            self.__tail = new_item
+        else:
+            if position > self.__len:
+                raise IndexError(f"Desired position {position}, "
+                                 f"but list's length is {self.__len}.")
+            elif position < 0:
+                raise IndexError
+            if position == 0:
+                self.__len += 1
+                new_item = LinkedListItem(value)
+                if not self.__head:
+                    self.__head = new_item
+                    self.__tail = new_item
+                else:
+                    new_item.set_next(self.__head)
+                    self.__head = new_item
+            elif position == self.__len:
+                self.__len += 1
+                new_item = LinkedListItem(value)
+                self.__tail.set_next(new_item)
+                self.__tail = new_item
+            else:
+                self.__len += 1
+                new_item = LinkedListItem(value)
+                current = self.__head
+                for i in range(position - 1):
+                    current = current.get_next()
+                next_item = current.get_next()
+                current.set_next(new_item)
+                new_item.set_next(next_item)
+        return self
+
+    def extend(self, arg_list: list, position=None):
+        for element in arg_list:
+            self.add(element, position)
+        return self
+
+    def first(self):
+        if self.__len == 0:
+            raise IndexError('Linked list is empty')
+        return self.__head.value
+
+    def last(self):
+        if self.__len == 0:
+            raise IndexError('Linked list is empty')
+        return self.__tail.value
+
+    def pop(self, index=None):
+        if index is None or index == self.__len - 1:
+            current = self.__head
+            while current.get_next().has_next():
+                current = current.get_next()
+            popped_element = current.get_next()
+            self.__tail = current
+            current.set_next(None)
+            self.__len -= 1
+        elif index > self.__len - 1:
+            raise IndexError(f"Desired index is {index},"
+                             f" but last element index is {self.__len - 1}.")
+        elif index == 0:
+            popped_element = self.__head
+            new_next_element = popped_element.get_next()
+            self.__head = new_next_element
+            self.__len -= 1
+        else:
+            current = self.__head
+            for _ in range(index - 1):
+                current = current.get_next()
+            popped_element = current.get_next()
+            new_next_element = popped_element.get_next()
+            current.set_next(new_next_element)
+            self.__len -= 1
+        return popped_element.value
+
+    def remove_last_occurrence(self, element):
+        if element not in self:
+            raise ValueError(f'No such element in LinkedList: {element}')
+        current = self.__head
+        index = 0
+        last_element_index = 0
+        for _ in range(self.__len):
+            if current.value == element:
+                last_element_index = index
+            current = current.get_next()
+            index += 1
+        self.pop(last_element_index)
+
+        return self
+        
---
+++ linked_list_class/test.py
@@ -0,0 +1,139 @@
+import unittest
+from linkedlist import LinkedList
+
+
+class TestLinkedList(unittest.TestCase):
+    def setUp(self):
+        self.items = LinkedList()
+
+    def test_add_get_item(self):
+        check_list = [1, 34, 'f', [231, 324, 1]]
+        for element in check_list:
+            self.items.add(element)
+
+        for i in range(len(check_list)):
+            self.assertEqual(self.items[i], check_list[i])
+
+        with self.assertRaises(IndexError):
+            var = self.items[-2]
+        with self.assertRaises(IndexError):
+            var = self.items[100]
+
+    def test_first_element(self):
+        with self.assertRaises(IndexError):
+            self.items.first()
+        check_list = [124, 436, 123, 54, [325, 1],
+                      [325, ['f', '325'], (3, 1)], 35.4]
+        first_value = (235, 'er')
+        self.items.add(first_value)
+        for value in check_list:
+            self.items.add(value)
+            self.assertEqual(self.items.first(), first_value)
+
+    def test_last_element(self):
+        with self.assertRaises(IndexError):
+            self.items.last()
+        check_list = [[325, 1], [325, ['f', '325'], (3, 1)], 35.4, 325]
+        for value in check_list:
+            self.items.add(value)
+            self.assertEqual(self.items.last(), value)
+
+    def test_extend(self):
+        check_list = [[1, 2, 3, 4], [12, 3245, 235, 'fr'],
+                      [35, 654, 2367, 12745678]]
+        j = 0
+        for list_element in check_list:
+            self.items.extend(list_element)
+            for i in range(len(list_element)):
+                self.assertEqual(self.items[i + j], list_element[i])
+            j += len(list_element)
+
+    def test_search_in(self):
+        check_list = [124, 325564, 'super', [231, '2', 1], [231, '2', 1],
+                      [[124, 346], (124, 1), '325ls/'], [731, '2', 1]]
+        for element in check_list:
+            self.items.add(element)
+        i = 0
+        for element in self.items:
+            self.assertEqual(element, check_list[i])
+            i += 1
+
+    def test_pop_int(self):
+        check_list = [1, 2, 346, 5474, '235', 12523,
+                      'dshghdak', '3246', [32545, [325], []], 326]
+        self.items.extend(check_list)
+        indices = [0, 5, 2, 3]
+        for index in indices:
+            self.assertEqual(self.items.pop(index), check_list.pop(index))
+        with self.assertRaises(IndexError):
+            self.items.pop(6)
+
+    def test_add_with_position(self):
+
+        mini_check_list = [1, 2, 3, 4]
+        check_add = [2364, 43, 215]
+        self.items.extend(check_add)
+        position = 0
+        for element in mini_check_list:
+            check_add.insert(position, element)
+            self.items.add(element, position)
+            self.assertEqual(self.items[position], check_add[position])
+            position += 1
+
+    def test_add_with_position_left_boundary(self):
+        with self.assertRaises(IndexError):
+            self.items.add(325, -1)
+
+    def test_add_with_position_right_boundary(self):
+        self.items.extend([12, 346, 214, 346542])
+        with self.assertRaises(IndexError):
+            self.items.add(325, 5)
+
+    def test_extend_with_position(self):
+        check_list = [12, 3245, 235, 'fr', 235, 543,
+                      35, 654, 2367, 12745678, 236]
+        mini_check_list = [[2, 3], [4, 5], [124, 'df', 346]]
+        position = 0
+        self.items.extend(check_list)
+        for element in mini_check_list:
+            for sub_element in element:
+                check_list.insert(position, sub_element)
+            self.items.extend(element, position)
+            self.assertEqual(self.items[position], check_list[position])
+            position += 1
+
+    def test_extend_with_position_left_boundary(self):
+        with self.assertRaises(IndexError):
+            self.items.extend([32532, 3521, '43'], -1)
+
+    def test_extend_with_position_right_boundary(self):
+        self.items.extend(['23564', 'ewr', '23455', 324567, [325, 235]])
+        with self.assertRaises(IndexError):
+            self.items.add(325, 6)
+
+    def test_first_last_check_with_pop(self):
+        check_list = [1, 2, 3, 4, 5, 234, 46, 123, 10]
+        indices = [0, 1, 5]
+        self.items.extend(check_list)
+        self.assertEqual(self.items.first(), check_list[0])
+        self.assertEqual(self.items.last(), check_list[-1])
+        for index in indices:
+            self.items.pop(index)
+            check_list.pop(index)
+            self.assertEqual(self.items.first(), check_list[0])
+            self.assertEqual(self.items.last(), check_list[-1])
+
+    def test_remove_last_occurrence(self):
+        check_list = [10, 12, 'x', 34, 10, 'x', 35, 'x']
+        self.items.extend(check_list)
+        check_list.reverse()
+        check_list.remove('x')
+        check_list.reverse()
+        self.items.remove_last_occurrence('x')
+        for i in range(len(check_list)):
+            self.assertEqual(self.items[i], check_list[i])
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
